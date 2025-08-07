0e4b947cc5cf48407c67f9a242754dc3a4460abf
tymfernandez <johntymothyfernandez@gmail.com>
2025-08-07 15:40:27 +0800

    merging

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,11 @@ import ApplicationForm from './ApplicationForm/applicationForm';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 py-8">
+    <Layout>
+      <Dashboard />
       <ApplicationForm />
-    </div>
-  )
+    </Layout>
+  );
 }
 
 export default App
