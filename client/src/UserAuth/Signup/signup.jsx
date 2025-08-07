d03b487f9beab4b640726780ca9aebb525caa121
tymfernandez <johntymothyfernandez@gmail.com>
2025-08-07 13:27:45 +0800

    fix bugs

--- client/src/UserAuth/Signup/signup.jsx
+++ client/src/UserAuth/Signup/signup.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "../../lib/authContext";
+import { useAuth } from "../../lib/AuthContext";
 
 export default function SignUp() {
   const [email, setEmail] = useState("");
